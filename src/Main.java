3f4fc727d06c990e681afa1e1b8e7a2064d782c8
hugorodsas <hugo.rodsas@educa.jcyl.es>
2026-02-13 09:15:23 +0100

    Gestion de Alumnos en Binario

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,48 @@
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        AlRepositorio repositorio = new AlRepositorio("alumnos.ser");
+        String  archivo = "alumnos.ser";
+        Scanner sc = new Scanner (System.in);
+        ArrayList<Alumno> lista = new ArrayList<>();
 
-        List<Alumno> alumnos = repositorio.leerAlumnos();
+        String opcion;
+        do{
+            System.out.println("Ingrese el nombre del alumno");
+            String nombre = sc.nextLine();
 
-        if (alumnos.isEmpty()) {
-            System.out.println("No hay alumnos");
-            return;
+            System.out.println("Ingrese la nota del alumno");
+            double nota = sc.nextDouble();
+            sc.nextLine();
+
+            lista.add(new Alumno(nombre,nota));
+
+            System.out.println("Desea ingresar un nuevo alumno (s/n)");
+            opcion = sc.nextLine();
+        }while(opcion.equalsIgnoreCase("s"));
+
+        Escritor escritor = new Escritor();
+        Lector lector = new Lector();
+        AlService service = new AlService(lista);
+
+        try{
+            escritor.guardar(lista, archivo);
+            System.out.println("Alumnos guardados");
+
+            ArrayList<Alumno> alumnos = lector.leer(archivo);
+
+            service.mostrarNotas();
+            service.mostrarOrdenadosporNota();
+            service.mostrarAprobados();
+            service.mostrarMejorAlumno();
+
+            /*System.out.println("Alumnos leidos del archivo:");
+            for (Alumno a : alumnos) {
+                System.out.println(a);
+            }*/
+        }catch(Exception e){
+            e.printStackTrace();
         }
-
-        AlService service = new AlService(alumnos);
-
-        service.mostrarNotas();
-        service.mostrarOrdenadosporNota();
-        service.mostrarAprobados();
-        service.mostrarMejorAlumno();
     }
 }
